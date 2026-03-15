244fe486388da40c953c8cfd7f4669aed1b23d44
codeugar <codeugar@gmail.com>
2026-03-15 12:56:16 +0800

    chore: remove unused scripting permission

--- src/manifest.ts
+++ src/manifest.ts
@@ -19,7 +19,7 @@ export default defineManifest({
             '48': 'icon48.png',
         },
     },
-    permissions: ['storage', 'activeTab', 'scripting', 'clipboardWrite'],
+    permissions: ['storage', 'activeTab', 'clipboardWrite'],
     host_permissions: ['https://generativelanguage.googleapis.com/*', 'https://data.similarweb.com/*'],
     background: {
         service_worker: 'src/background/index.ts',
